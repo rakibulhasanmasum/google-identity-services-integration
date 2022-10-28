fb8f5ba00c78f68c83bab06434ff4e968cf6a5b4
r_hasan <rakibul.hasan@trucklagbe.com>
2022-10-28 14:06:31 +0600

    get user credentials

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "CLIENT_ID": "445565341261-ri8tkio3kt4dbd8769qe1dh3ha2lgb2f.apps.googleusercontent.com"
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import jwtDecode from 'jwt-decode';
+import { useEffect } from 'react';
+import { Navigate, redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router'
 import './App.css';
-
+import env from 'react-dotenv'
 function App() {
+
+  function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    console.log(jwtDecode(response.credential));
+    // <Navigate to="/dashboard" />
+    // let navigate = useNavigate();
+    // navigate("/dashboard");
+    // window.open('http://localhost:3000/dashboard')
+    // <Redirect to="/dashboard" />
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: env.CLIENT_ID,
+      callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, [])
+
+  // google.accounts.id.prompt(); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="buttonDiv"></div>
     </div>
   );
 }
